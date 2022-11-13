3428a633ada00a4b45a6f0945071e30b7f832f92
EngPauloSergio <engpaulosergiofilho@gmail.com>
2022-11-13 16:38:54 -0300

    aparentemente eu terminei

--- paulo/monthly.py
+++ paulo/monthly.py
@@ -1,11 +1,23 @@
 import pandas as pd
-import math
+import numpy as np
 
 # desafio -> somar o volume mensal
 # cada transação tem um produto e um volume
 # é necessário portanto somar o volume de cada transação para que se tenha o volume mensal
 
-df = pd.read_csv(r"paulo\Dados\all_sales.csv")
+
+# abrindo e salvando como um dataframe
+df = pd.read_csv(r'dados\all_sales.csv')
 
 def monthly(df):
-    pass
+    # criando df somente com produtos e o mês
+    month_and_product = df.loc[:, 'month_year':'prod_15']
+
+    # somando o numero de transações por mês
+    sum_of_month = month_and_product.groupby(['month_year']).sum()
+
+    return sum_of_month
+
+data = monthly(df)
+
+print(data)
