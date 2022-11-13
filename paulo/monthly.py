e984b3f77252c8ccb11fa1806e5f82a80ba9fd33
EngPauloSergio <engpaulosergiofilho@gmail.com>
2022-11-13 14:41:15 -0300

    to fazendo um teste, o nome aparece vermelho e quero ver se é só pq ele não ta atualizado

---
+++ paulo/monthly.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import math
+
+# desafio -> somar o volume mensal
+# cada transação tem um produto e um volume
+# é necessário portanto somar o volume de cada transação para que se tenha o volume mensal
+
+df = pd.read_csv(r"paulo\Dados\all_sales.csv")
+
+def monthly(df):
+    pass
